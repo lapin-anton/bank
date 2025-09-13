7ed60c3e809588bf0cc54e52eb953239458af3f5
a.lapin <a.lapin@cloudcom.ru>
2025-09-13 21:26:55 +0300

    add blocker

---
+++ clients/blocker/src/main/java/ru/yandex/practicum/bank/client/blocker/api/BlockerClient.java
@@ -0,0 +1,37 @@
+package ru.yandex.practicum.bank.client.blocker.api;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import ru.yandex.practicum.bank.client.blocker.model.CashCheckDto;
+import ru.yandex.practicum.bank.client.blocker.model.ResultCheckDto;
+import ru.yandex.practicum.bank.client.blocker.model.TransferCheckDto;
+import ru.yandex.practicum.bank.common.resilience.ResilienceExecutor;
+
+@Component
+@RequiredArgsConstructor
+public class BlockerClient {
+
+    private final BlockerApi blockerApi;
+
+    private final ResilienceExecutor resilience;
+
+    public ResultCheckDto checkCash(CashCheckDto dto) {
+        return resilience.execute(
+                () -> blockerApi.checkCash(dto),
+                this::defaultResult);
+    }
+
+    public ResultCheckDto checkTransfer(TransferCheckDto dto) {
+        return resilience.execute(
+                () -> blockerApi.checkTransfer(dto),
+                this::defaultResult);
+    }
+
+    private ResultCheckDto defaultResult() {
+        ResultCheckDto resultCheckDto = new ResultCheckDto();
+
+        resultCheckDto.setResult(false);
+
+        return resultCheckDto;
+    }
+}
